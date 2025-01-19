34861b59f85d3b68b14eacf3c031b76a84d40675
Bohdan Savshak <bohdan.savshak@gmail.com>
2025-01-19 21:50:47 +0200

    springjava-format
    - Good code
    - Hard to install Intellij Plugin.
    - easy to configure maven plugin with spring format style.
    - Good Support for new java features.
    - Maintained by Spring

--- src/main/java/com/bohdansavshak/java16_jep_394_pattern_matching_for_instance_of/Exercise1.java
+++ src/main/java/com/bohdansavshak/java16_jep_394_pattern_matching_for_instance_of/Exercise1.java
@@ -3,7 +3,7 @@ package com.bohdansavshak.java16_jep_394_pattern_matching_for_instance_of;
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		String str = "my string";
+		String str = "my string1";
 		Object obj = str;
 		if (obj instanceof String s) {
 			System.out.println(s);
